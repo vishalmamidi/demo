782bc9904467b030b0c916be5f9997c5bc84555f
vishalmamidi <mamidivishalsa@gmail.com>
2022-01-29 17:21:27 +0530

    init

---
+++ src/main/java/com/vishalmamidi/dummyrestlogger/DummyRestLoggerApplication.java
@@ -0,0 +1,13 @@
+package com.vishalmamidi.dummyrestlogger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyRestLoggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyRestLoggerApplication.class, args);
+	}
+
+}
